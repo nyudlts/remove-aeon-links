dff7933c60c8657f9f0f438c608dcb286820f80d
Donald Mennerich <don.mennerich@nyu.edu>
2023-03-01 06:20:25 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module aeon-destroyer
+
+go 1.19
+
+require (
+	github.com/lestrrat-go/libxml2 v0.0.0-20201123224832-e6d9de61b80d // indirect
+	github.com/nyudlts/go-aspace v0.3.8-0.20221212180752-3a41d26e4f7c // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	go_aspace "github.com/nyudlts/go-aspace"
+	"regexp"
+)
+
+var (
+	aspace      *go_aspace.ASClient
+	config      string
+	environment string
+	repository  int
+	resource    int
+	test        bool
+	aeonPtn     = regexp.MustCompile("aeon.library.nyu.edu")
+)
+
+func init() {
+	flag.StringVar(&config, "config", "", "")
+	flag.StringVar(&environment, "env", "", "")
+	flag.IntVar(&repository, "repo", 0, "")
+	flag.IntVar(&resource, "res", 0, "")
+	flag.BoolVar(&test, "test", false, "")
+}
+
+func main() {
+	flag.Parse()
+
+	//create an ASClient
+	var err error
+	aspace, err = go_aspace.NewClient(config, environment, 20)
+	if err != nil {
+		panic(err)
+	}
+
+	//get the digital objects attached to a resource
+	doURIs, err := aspace.GetDigitalObjectIDsForResource(repository, resource)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := range doURIs {
+		result, err := hasAeonLinks(doURIs[i])
+		if err != nil {
+			//log the error
+			continue
+		}
+
+		if result == false {
+			//skip the do
+			continue
+		}
+
+		err := removeAeonLink(doURIs[i])
+	}
+}
+
+func removeAeonLink(string) error {
+	return nil
+}
+
+func hasAeonLinks(doURI string) (bool, error) {
+	repoID, doID, err := go_aspace.URISplit(doURI)
+	if err != nil {
+		return false, err
+	}
+
+	do, err := aspace.GetDigitalObject(repoID, doID)
+	if err != nil {
+		return false, err
+	}
+
+	if len(do.FileVersions) == 1 && aeonPtn.MatchString(do.FileVersions[0].FileURI) {
+		return true, nil
+	}
+
+	return false, nil
+}
